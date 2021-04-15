1425bce3e71fd60578d92850afcb8c83c4fee6ba
Mitchell Kucy <kucy1221@gmail.com>
2021-04-15 20:49:00 +0000

    Added Bootstrap, jquery, & popper. Added Styling to website

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,30 +1,40 @@
 class ReviewsController < ApplicationController
+    before_action :set_review, only: [:show, :destroy]
+
 
     def show
-        @review = Review.find(params[:id])
     end
 
     def index
-        @reviews = Review.all
+        @reviews = Review.all()
     end
     
     def new
-        @review = Review.new
+        @review = Review.new()
     end
     
     def create
-        @review = Review.new(params.require(:review).permit(:title, :body))
-        if @review.save
+        @review = Review.new(review_params())
+        if @review.save()
             flash[:notice] = "Review Posted Successfully."
-            redirect_to reviews_path(@review)
+            redirect_to review_path(@review)
         else
             render 'new'
         end
     end
     
     def destroy
-        @review = Review.find(params[:id])
-        @review.destroy
+        @review.destroy()
         redirect_to reviews_path
+    end
+    
+    private 
+    
+    def set_review
+        @review = Review.find(params[:id])
+    end
+    
+    def review_params
+        params.require(:review).permit(:title, :body)
     end
 end
