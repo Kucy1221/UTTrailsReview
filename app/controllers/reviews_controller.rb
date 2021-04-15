8a6818914e7ac56e945822f1e4cf152f6875cb7f
Mitchell Kucy <kucy1221@gmail.com>
2021-04-15 00:07:12 +0000

    Implemented create and delete functionality for reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -7,4 +7,24 @@ class ReviewsController < ApplicationController
     def index
         @reviews = Review.all
     end
+    
+    def new
+        @review = Review.new
+    end
+    
+    def create
+        @review = Review.new(params.require(:review).permit(:title, :body))
+        if @review.save
+            flash[:notice] = "Review Posted Successfully."
+            redirect_to reviews_path(@review)
+        else
+            render 'new'
+        end
+    end
+    
+    def destroy
+        @review = Review.find(params[:id])
+        @review.destroy
+        redirect_to reviews_path
+    end
 end
