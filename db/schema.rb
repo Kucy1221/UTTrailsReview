1a6c8adc24e14391d30d9ad859a730299fdfb910
Mitchell Kucy <kucy1221@gmail.com>
2021-04-18 19:57:07 +0000

    Added User model to db

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_14_161210) do
+ActiveRecord::Schema.define(version: 2021_04_18_195105) do
 
   create_table "reviews", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "time_created"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
