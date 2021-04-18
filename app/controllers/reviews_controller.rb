d88c37a4a60b3f4ffebf8f480ae0f56c950f6a53
Mitchell Kucy <kucy1221@gmail.com>
2021-04-18 15:49:40 +0000

    Layout fixes, refactoring

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,8 @@
 class ReviewsController < ApplicationController
+  before_action :set_review, only: [:show, :destroy]
+
 
     def show
-        @review = Review.find(params[:id])
     end
 
     def index
@@ -13,7 +14,7 @@ class ReviewsController < ApplicationController
     end
     
     def create
-        @review = Review.new(params.require(:review).permit(:title, :body))
+        @review = Review.new(review_params)
         if @review.save
             flash[:notice] = "Review Posted Successfully."
             redirect_to reviews_path(@review)
@@ -23,8 +24,17 @@ class ReviewsController < ApplicationController
     end
     
     def destroy
-        @review = Review.find(params[:id])
         @review.destroy
         redirect_to reviews_path
     end
 end
+
+  private
+
+  def set_review
+    @review = Review.find(params[:id])
+  end
+
+  def review_params
+    params.require(:review).permit(:title, :body)
+  end
