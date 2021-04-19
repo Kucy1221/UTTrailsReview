6d6da8af569f4e6f90a247d8de182ff59ec692fe
Mitchell Kucy <kucy1221@gmail.com>
2021-04-19 04:55:10 +0000

    Added sessions for users

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,11 +4,22 @@ class SessionsController < ApplicationController
     end
     
     def create
-
+        user = User.find_by(email: params[:session][:email].downcase())
+        if user and user.authenticate(params[:session][:password])
+            session[:user_id] = user.id
+            flash[:notice] = "Logged in successfully"
+            redirect_to user
+        
+        else
+            flash.now[:alert] = "Incorrect login details"
+            render 'new'
+        end
     end
     
     def destroy
-    
+        session[:user_id] = nil
+        flash[:notice] = "You are now logged out."
+        redirect_to root_path
     end
 
 end
