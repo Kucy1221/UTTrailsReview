60df6db21289644a3afb4e12e6bbef1b4650c787
Mitchell Kucy <kucy1221@gmail.com>
2021-04-19 01:06:44 +0000

    Users can now sign up, and the sign up button on the homepage is working.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,22 @@ class UsersController < ApplicationController
     def new
         @user = User.new()
     end
+    
+    def create
+    
+        @user = User.new(user_params())
+        if @user.save()
+            flash[:notice] = "Hi, " + @user.username + ", Welcome to Utah Trail Review!"
+            redirect_to root_path
+        else
+            render 'new'
+        end
+    
+    end
+    
+    private
+    def user_params
+        params.require(:user).permit(:username, :email, :password)
+    end
 
 end
