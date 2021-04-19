977796ebab41328ae621a21d00ccda3356b0203f
Mitchell Kucy <kucy1221@gmail.com>
2021-04-19 05:23:14 +0000

    - Added current_user and logged_in? helper methods.
    - Modified the navigation partial and utilized these methods to display links based on logged in state.
    
    - Modified the users controller create action so users' are logged in once they sign-up.
    
    - Added set_user method and used it as before_action for show, edit and update actions in users controller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
+    before_action :set_user, only: [:show, :edit, :update]
 
     def show
-        @user = User.find(params[:id])
         @reviews = @user.reviews.paginate(page: params[:page], per_page: 10)
     end
 
@@ -14,11 +14,9 @@ class UsersController < ApplicationController
     end
     
     def edit
-        @user = User.find(params[:id])
     end
         
     def update
-        @user = User.find(params[:id])
         if @user.update(user_params)
             flash[:notice] = "Your account was successfully updated."
             redirect_to user_path(@user.id)
@@ -28,9 +26,9 @@ class UsersController < ApplicationController
     end
     
     def create
-    
         @user = User.new(user_params())
         if @user.save()
+            session[:user_id] = @user.id
             flash[:notice] = "Hi, " + @user.username + ", Welcome to Utah Trail Review!"
             redirect_to root_path
         else
@@ -44,6 +42,10 @@ class UsersController < ApplicationController
     private
     def user_params
         params.require(:user).permit(:username, :email, :password)
+    end
+    
+    def set_user
+        @user = User.find(params[:id])
     end
 
 end
