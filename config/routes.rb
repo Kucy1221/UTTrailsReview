a6973aa7e617a6b1182116b7871d6dc9c2fa3aa6
Mitchell Kucy <kucy1221@gmail.com>
2021-04-19 04:35:25 +0000

    created login form. Not yet functional

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
     root 'pages#home'
     get 'about', to: 'pages#about'
     resources :reviews, only: [:show, :index, :new, :create, :destroy] 
+    
     get 'signup', to: 'users#new'
     resources :users, except: [:new]
+    
+    get 'login', to: 'sessions#new'
+    post 'login', to: 'sessions#create'
+    delete 'logout', to: 'sessions#destroy'
+    
 end
