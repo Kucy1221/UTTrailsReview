90cd73dcee172bdc494e25975ea08f971ee3982c
Mitchell Kucy <kucy1221@gmail.com>
2021-04-25 20:19:09 +0000

    Trail.new_lookup() now returns a list of trails that have a matching name from the AGRC API

--- app/models/trail.rb
+++ app/models/trail.rb
@@ -5,30 +5,36 @@ class Trail < ApplicationRecord
     
     def self.new_lookup(trailName)
         params = {}
-        
         @api_key = Rails.application.credentials.agrc[:server_agrc_key]
         
-        #?predicate=primaryname+LIKE+'Kolob%25'
-        params['predicate'] = "primaryname LIKE '#{trailName}'"
+        #AGRC stores primaryname as spaced capitalized words EG 'Devils Castle Trail'
+        params['predicate'] = "primaryname LIKE '%#{trailName.downcase().titleize()}%'"
         params['apiKey'] = @api_key
         
         api_url = "https://api.mapserv.utah.gov/api/v1/search/recreation.trails_and_pathways/primaryname, id, hikedifficulty, designateduses, recreationarea"
     
         uri = URI(URI.encode(api_url))
         uri.query = URI.encode_www_form(params)
-        #return(uri)
+        #puts(uri)
         res = Net::HTTP.get_response(uri)
-        #raise AGRCGeocoderException.new("Received HTTP status #{res.code}") if res.code.to_i != 200
+        raise AGRCGeocoderException.new("Received HTTP status #{res.code}") if res.code.to_i != 200
     
         obj = JSON.parse(res.body)
-        # #raise AGRCGeocoderException.new(obj['message']) if obj['status'] != 200
-        return obj
-        {
-          :primaryname => obj['result']['primaryname'],
-          :id => obj['result']['id'],
-          :hikedifficulty => obj['result']['hikedifficulty']
-        }
+        raise AGRCGeocoderException.new(obj['message']) if obj['status'] != 200
+        puts obj
         
-        
-    end    
+        trails = []
+        obj['result'].each() do |trail|
+            newTrail = {
+              :primaryname => trail['attributes']['primaryname'],
+              :id => trail['attributes']['id'],
+              :hikedifficulty => trail['attributes']['hikedifficulty'],
+              :designateduses => trail['attributes']['designateduses'],
+              :recreationarea => trail['attributes']['recreationarea']
+            }
+            trails.append(newTrail)
+        end
+        return trails
+    end
+    class AGRCGeocoderException < Exception; end
 end
